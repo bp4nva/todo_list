796e65a522a5affd928785a5ccd6176c03e8db7c
bp4nva <bp4nvacom@gmail.com>
2024-11-29 18:54:33 -0500

    Adding to list of headings.

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,24 +2,45 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
+
+    const handleAddTodo = () => {
+        console.log("handleAddToDo: headingInput: " + headingInput)
+        if (headingInput.trim() !== '') {
+          // todos is an array of heading/list {heading: <heading>, lists: []},
+          // items, where heading is the main todo category - "Groceries",
+          // for example, and the list is the specific grocery items to get
+          setTodos([...todos, { heading: headingInput, lists: [] }]);
+          setHeadingInput('');
+        }
+      };
 
   return (
     <>
       <div className="todo-container">
         <h1 className="title">My Todo List</h1>
         <div className="input-container">
-          <input
+        <input
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
-          />
-          <button className="add-list-button">Add Heading</button>
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
+        />
+        <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
-        
+        {todos.map((todo, index) => (
+            <div key={index} className="todo-card">
+            <div className="heading_todo">
+                <h3>{todo.heading}</h3> {/* Display the heading here */}
+                <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+            </div>
+            </div>
+          ))}        
       </div>
     </>
   );
